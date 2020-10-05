a2b9aade10c24a51dbf402b63abdd21c6ac07269
Roxana <filip.roxana95@gmail.com>
2020-10-05 20:19:05 +0300

    Primul Program.

---
+++ lab1-ex0/src/main/java/ro/utcluj/lab1/ex1/PrimulProiect.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.lab1.ex1;
+
+/**
+ *
+ * @author Roxana
+ */
+public class PrimulProiect {
+   
+    public static void main (String[]args) {
+        System.out.println("Hello git");
+    }
+    
+}
